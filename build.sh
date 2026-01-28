83ea3d602e6f47e5def5701b63dcac8b63a2d466
Somesh Chandra Behera <someshbehera098@gmail.com>
2026-01-28 23:06:45 +0530

    Reduce build memory usage

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
