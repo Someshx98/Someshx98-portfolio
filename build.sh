4d4c788d2f0c8084da7ffbc833bee59d6537f53e
Somesh Chandra Behera <someshbehera098@gmail.com>
2026-01-29 06:55:22 +0530

    Force regenerate static files on deploy

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -o errexit
 
 pip install --no-cache-dir -r requirements.txt
 
+# Remove old static files
+rm -rf staticfiles
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createadmin
