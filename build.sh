ae757c502f7412ca5a108d26036213bbb232dce5
Somesh Chandra Behera <someshbehera098@gmail.com>
2026-01-29 07:06:44 +0530

    Fix static files collection and navigation

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@ set -o errexit
 
 pip install --no-cache-dir -r requirements.txt
 
-# Remove old static files
-rm -rf staticfiles
-
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
 python manage.py createadmin
