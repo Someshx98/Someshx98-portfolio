47219fba2ac84582d96a84d9aa37ad1c2c14bb2d
Somesh Behera <someshbehera0908@gmail.com>
2026-01-29 07:16:45 +0530

    Remove clear option from collectstatic command

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install --no-cache-dir -r requirements.txt
 
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createadmin
